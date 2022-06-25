a6f286c19883299c1f8145cbe7a678eec9919462
Wallacy Caike <103141693+wallacycaike@users.noreply.github.com>
2022-06-24 22:03:28 -0300

    Projeto finalizado.
    Site responsivo com o uso de JS, dando inicio a minha prática com essa linguagem de programação.

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+// MOSTRAR MENU
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MOSTRAR MENU =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== ESCONDER MENU =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// REMOVER MENU MOBILE
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// ANIMAÇÃO SCROLL REVEAL
+const sr = ScrollReveal({
+    distance: '90px',
+    duration: 3000,
+})
+
+sr.reveal(`.home__data`, {origin: 'top', delay: 400})
+sr.reveal(`.home__img`, {origin: 'bottom', delay: 600})
+sr.reveal(`.home__footer`, {origin: 'bottom', delay: 400})
